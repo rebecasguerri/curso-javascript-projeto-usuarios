5261e7a012a588e78f38f065a1b0cbc8499dc715
rebecasguerri <rebecasguerri@gmail.com>
2022-07-26 13:26:55 -0300

    update

--- controllers/usercontroler.js
+++ controllers/usercontroler.js
@@ -2,39 +2,45 @@ class UserController {
     constructor(formId, tableId){
         this.formEL= document.getElementById(formId);
         this.tableId= document.getElementById(tableId);
+
         this.onSubmit();
+      
     }
     onSubmit(){ 
        this.formEL.addEventListener("submit", event=>{
             event.preventDefault();
            
             let values = this.getValues();
+            
 
             this.getPhoto((content)=>{
-                 values.photo =content;
-                 this.addLine(values);
-            }) ;
-          
+                values.photo = content;
+
+                this.addLine(values);
+            });
+ 
          });
     }
     getPhoto(callback){
-        let fileReader = new FileReader()
-        let elements =  [...this.formEL.elements].filter(item =>{
-            if (item.name === 'photo') {
-                return item;
-            }
-        });
-        let file = elements[0].files[0]
-        fileReader.onload = () =>{
-           
-           callback(fileReader.result)
-        };
+        let fileReader = new FileReader();
 
-        fileReader.readAsDataURL(file);
+        let elements = [...this.formEL.elements].filter(item=>{
+           if(item.name === 'photo'){
+             return item
+           } 
+
+        });
+        let file = elements[0].files[0];
+
+
+        fileReader.onload=( ) =>{
+            callback(fileReader.result);
+       };
+       fileReader.readAsDataURL(file);
     }
     getValues(){
         let user = {};
-       // this.formEL.elements
+      
          [...this.formEL.elements].forEach(function(field, index){
 
             if(field.name == "gender"){
@@ -45,7 +51,7 @@ class UserController {
             }else{
                 user[field.name]= field.value;
             }
-            //console.log(field.name, field.id);
+           
         });
 
          return new User (
@@ -63,12 +69,8 @@ class UserController {
     }
 
      addLine(dataUser){
-        // console.log("addline", dataUser);
-         //console.log(dataUser)
-        // var tr= document.createElement('tr');
-    
-         this.tableEL.innerHTML =`
-         <tr>
+         this.tableEL.innerHTML=
+         `<tr>
          <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
          <td>${dataUser.name}</td>
          <td>${dataUser.email}</td>
