0f40d3467e6ae059a524af05c823df094e833ff1
rebecasguerri <rebecasguerri@gmail.com>
2022-07-27 14:10:03 -0300

    update

--- classes/utils.js
+++ classes/utils.js
@@ -1,5 +1,5 @@
 class Ultils{
    static dateFormat(date){
-        return date.getDate()+"/"+ (date.getMonth()+1)+ date.getFullYear()+" "+ date.getHours()+":"+date.getMinutes();
+        return date.getDate()+"/"+ (date.getMonth()+1)+"/"+ date.getFullYear()+" "+ date.getHours()+":"+date.getMinutes();
     }
 }
