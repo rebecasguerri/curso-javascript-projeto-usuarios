dfb3b5713bbbbccb889363db17b9f0fa7566a124
rebecasguerri <rebecasguerri@gmail.com>
2022-07-31 19:57:40 -0300

    update

--- controllers/usercontroler.js
+++ controllers/usercontroler.js
@@ -4,7 +4,13 @@ class UserController {
         this.tableEl= document.getElementById(tableId);
 
         this.onSubmit();
+        this.onEdit();
       
+    }
+    onEdit(){
+        document.querySelector("#box-user-update .btn-cancel").addEventListener("click", e=>{
+            this.showPanelCreate();
+        });
     }
     onSubmit(){ 
        this.formEl.addEventListener("submit", event=>{
@@ -113,14 +119,28 @@ class UserController {
         <td>${(dataUser.admin) ?"Sim" : "Não"}</td>
         <td>${Ultils.dateFormat(dataUser.register)}</td>
         <td>
-          <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+          <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
           <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
         </td>
         </tr>
         `;
+        tr.querySelector(".btn-edit").addEventListener("click", e=>{
+           console.log( "a parada aconteceu", JSON.parse(tr.dataset.user));
+
+           this.showPanelUpdate();
+        })
          this.tableEl.appendChild(tr);
          this.uppdateCount()
          
+    }
+
+    showPanelCreate(){
+        document.querySelector('#box-user-create').style.display = "block";
+           document.querySelector('#box-user-update').style.display = "none";
+    }
+    showPanelUpdate(){
+        document.querySelector('#box-user-create').style.display = "none";
+           document.querySelector('#box-user-update').style.display = "block";
     }
     uppdateCount(){
         let numberUsers = 0;
